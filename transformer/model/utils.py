ee4c327ae4163416da48307b3d19ef5bda050d3a
scotty110 <none@none>
2022-09-26 15:24:41 -0400

    add these

--- transformer/model/utils.py
+++ transformer/model/utils.py
@@ -3,6 +3,7 @@ Make transformer parts
 https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 '''
 
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -62,3 +63,24 @@ class multi_head(nn.Module):
         m_attention = self.drop1(self.w1(heads)) 
         return m_attention
 
+
+class PositionalEncoding(nn.Module):
+    # https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+    def __init__(self, d_model:int, dropout:float=0.1, max_len:int=5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
