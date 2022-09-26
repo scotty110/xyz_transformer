f5b5eb1162f714e7d04f4d47d852a970daae17fc
scotty110 <none@none>
2022-09-26 15:13:56 -0400

    Time to debug

--- transformer/model/model.py
+++ transformer/model/model.py
@@ -1,11 +1,32 @@
 '''
 Make transformer parts
 https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+https://arxiv.org/abs/1706.03762
 '''
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from decoder import decoder
+from encoder import encoder
+from utils import positional_encoder
 
+class transformer(nn.Module):
+    def __init__(self, d_shape:int):
+        # Big Layers 
+        self.Pos_Encoding = positional_encoder 
+        self.Encoder = encoder( d_shape )
+        self.Decoder = decoder( d_shape )
+        
+        # Output Layers
+        self.w = nn.Linear( d_shape, 1 )
+
+    def forward( in_emb, out_emb ):
+        e_output = self.Encoder( in_emb )
+        d_output = self.Decoder( out_emb, e_output )
+
+        # Output Layer
+        return F.softmax( self.w( d_output ), dim=1 )
+        
 
