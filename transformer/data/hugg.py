0c8c4c0f17cd7cc0ad9b93dd9cc388c400dffc38
scotty110 <none@none>
2022-10-15 11:53:34 -0400

    finally figured out massive text block

--- transformer/data/hugg.py
+++ transformer/data/hugg.py
@@ -4,7 +4,7 @@ https://huggingface.co/docs/datasets/v1.4.0/index.html
 '''
 
 from datasets import load_dataset
-from transformers import pipeline 
+from transformers import pipeline, PreTrainedTokenizerFast
 from torch.utils.data import Dataset, DataLoader
 import torch
 import random
@@ -15,31 +15,48 @@ class wiki_loader():
         self.ds = load_dataset('wikitext', 'wikitext-103-v1', split='train')
 
         pretrained_model = 'bert-base-uncased'
-        self.feature_extraction = pipeline('feature-extraction', model=pretrained_model, tokenizer=pretrained_model)
+        self.feature_extraction = pipeline('feature-extraction', model=pretrained_model, tokenizer=pretrained_model, truncation=True, device=0)
+
+    def format_text(self, txt):
+        if len(txt)>2:
+            t_split = txt.split(' ')
+            t_split = list(filter( lambda x: x != '', t_split))
+            if len(t_split) > 512:
+                print(txt)
+                t_split = t_split[:512]
+            return ' '.join(t_split)
+        return txt
+
 
     def make_mask(self, txt:str) -> str:
         if len(txt)>2:
             t_split = txt.split(' ')
-            t_split = list(filter( lambda x: x != '', t_split))
+            #print(len(t_split))
+            #t_split = list(filter( lambda x: x != '', t_split))
+            #if len(t_split) > 512:
+            #    t_split = t_split[:512]
             n = random.randrange(len(t_split))
             t_split[n] = '[MASK]'
             return ' '.join(t_split)
-         
         return txt
 
     def __len__(self) -> int:
         return len(self.ds)
 
     def __getitem__(self, index) -> tuple:
-        y_txt = self.ds[index]['text']
+        y_txt = self.format_text( self.ds[index]['text'] )
         x_txt = self.make_mask(y_txt)
         #print(y_txt, x_txt)
         y_ext = torch.Tensor( self.feature_extraction(y_txt) )
         x_ext = torch.Tensor( self.feature_extraction(x_txt) )
+
+        if y_ext.shape[1] > 512:
+            y_ext = y_ext[0][:512][:]
+            x_ext = x_ext[0][:512][:]
     
         y = torch.zeros(1,512,768)
         x = torch.zeros(1,512,768)
-        
+       
         y[0][:y_ext.shape[1]][:] = y_ext
         x[0][:x_ext.shape[1]][:] = x_ext
     
@@ -48,10 +65,10 @@ class wiki_loader():
         return (x, y, torch.Tensor([1]) )
 
 t = wiki_loader()
-print(t[1][0].shape)
-print(t[1][1].shape)
-t_dl = DataLoader( t, batch_size=4, shuffle=True,) 
+t_dl = DataLoader( t, batch_size=128, shuffle=True,) 
 x = iter(t_dl)
 n_x = next(x)
-print(n_x[0].shape, n_x[2].shape)
+while n_x != None:
+    print(n_x[0].shape, n_x[2].shape)
+    n_x = next(x)
 print('Done')
