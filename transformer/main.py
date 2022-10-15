b084e5c03ecb22427789f780438c4bcba894c59d
scotty110 <none@none>
2022-10-15 16:06:24 -0400

    Basic loop done

--- transformer/main.py
+++ transformer/main.py
@@ -3,6 +3,7 @@ Load Dataset and Model
 '''
 import time
 import torch
+import torch.nn as nn
 
 from transformer.data import get_datasets
 from transformer.model import get_model 
@@ -12,10 +13,17 @@ if __name__ == '__main__':
     
     train_dl, val_dl = get_datasets(64)
 
+    '''
+    Model, optimizer, loss func
+    '''
     model = get_model(768)
     model = model.to(device, torch.half)
 
+    opt = torch.optim.Adam( model.parameters(), 0.00001 )
+    loss_func = nn.MSELoss().to(device)
+
     start_t = time.time()
+
     '''
     tdl = iter(train_dl)
     tdl_t = next(tdl)
@@ -26,11 +34,24 @@ if __name__ == '__main__':
     y = y.to(device, torch.half)
     m = m.to(device, torch.half)
     '''
+
     x = torch.ones(64,512,768).to(device, torch.half)
     y = torch.ones(64,512,768).to(device, torch.half)
     m = torch.ones(64,1).to(device, torch.half)
-    
-    m_a = model(x,y) 
+
+    epochs = 20
+
+    for e in range(epochs):
+        opt.zero_grad()
+        model.half()
+        output = model(x,y)
+        loss = loss_func(output, m)
+        print("Loss: {}".format(loss.item()))
+
+        # Learn
+        loss.backward()
+        model.float()
+        opt.step()
 
 
     '''
